86f4335ad232fc8bda3de1d7b6dbab713f1a7b31
RumeysaAy <rumeeysaay@gmail.com>
2024-02-19 13:27:23 +0300

    Changing display items

--- Assets/Scripts/WeaponInventory.cs
+++ Assets/Scripts/WeaponInventory.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WeaponInventory : MonoBehaviour
 {
+    public Sprite[] bigIcons;
+    public Image bigIcon;
+    public string[] titles;
+    public Text title;
+    public string[] descriptions;
+    public Text description;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +26,9 @@ public class WeaponInventory : MonoBehaviour
 
     public void ChooseWeapon(int weaponNumber)
     {
-        Debug.Log(weaponNumber);
+        bigIcon.sprite = bigIcons[weaponNumber];
+        title.text = titles[weaponNumber];
+        description.text = descriptions[weaponNumber];
     }
 }
 
