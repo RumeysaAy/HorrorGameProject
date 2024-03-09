447015ad5607c72d203cf69d6160802def95d35b
RumeysaAy <rumeeysaay@gmail.com>
2024-03-09 16:03:25 +0300

    Displaying pickups

--- Assets/Scripts/PickupsScript.cs
+++ Assets/Scripts/PickupsScript.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PickupsScript : MonoBehaviour
 {
     // vurulan her şeyi kaydetmemiz gerekiyor
     private RaycastHit hit; // vurulan herhangi bir nesneyi depolayacak
     public LayerMask excludeLayers; // seçilen Layer'lar görmezden gelinir. sadece pickups layer'ını seçmedim. seçilen katmanlardaki nesneler algılanmayacak
+    public GameObject pickupPanel; // baktığımız silahı gösteren panel
+
+    public Image mainImage; // ışının çarptığı silahın resminin koyulacağı yer
+    public Sprite[] weaponIcons; // silahların resmi
+    public Text mainTitle; // ışının çarptığı silahın adının koyulacağı yer
+    public string[] weaponTitles; // silahların adı
 
     private int objID = 0; // hangi silah türüne vurduğumuza bağlı değişecek (WeaponType.cs)
 
     // Start is called before the first frame update
     void Start()
     {
+        pickupPanel.SetActive(false);
 
     }
 
@@ -28,10 +36,27 @@ public class PickupsScript : MonoBehaviour
             // yalnızca weapon etiketi olan nesneleri tespit ettiğimden emin olmak istiyorum.
             if (hit.transform.gameObject.CompareTag("weapon")) // yalnızca bir silahsa
             {
+                // ışının çarptığı nesne silahsa panel açılsın
+                pickupPanel.SetActive(true);
+
                 // hangi silahı işaret ettiğimizi tespit edebilmek için WeaponType.cs dosyasını kullanacağız.
                 objID = (int)hit.transform.gameObject.GetComponent<WeaponType>().chooseWeapon;
-                Debug.Log("Hit weapon: " + objID);
+                // ışının çarptığı silaha bağlı olarak başlığın ve görselin değişmesi için
+                mainImage.sprite = weaponIcons[objID];
+                mainTitle.text = weaponTitles[objID];
             }
+            else
+            {
+                // pickups katmanında fakat etiketi weapon olmayan nesne (item)
+                pickupPanel.SetActive(false);
+            }
+        }
+        else
+        {
+            // katmanı pickups değilse 
+            pickupPanel.SetActive(false);
         }
     }
 }
+
+// bu dosya FirstPersonCharacter nesnesine bileşen olarak eklenmiştir.
